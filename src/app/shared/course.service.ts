e0bb2943f786e59e9534bbb16320f56766744cb7
Raman <dzin13@tut.by>
2017-12-26 00:02:17 +0300

    added OnPush and aot

--- src/app/shared/course.service.ts
+++ src/app/shared/course.service.ts
@@ -11,8 +11,7 @@ export class CourseService {
     return this.courses;
   }
 
-  createItem(id, title, duration, date, description) {
-    let item = {id, title, duration, date, description};
+  createItem(item: Course) {
     this.courses.push(item);
   }
 
