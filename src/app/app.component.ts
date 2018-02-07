b289c763784baba79432c8d8d4b8d465d709f5da
Raman <dzin13@tut.by>
2018-02-07 09:17:24 +0300

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
+import { Router } from '@angular/router';
 
 import { AuthService } from './shared/auth.service';
+import { User } from './shared/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit, OnDestroy {
-  isAuthorized: boolean;
   _subscription: Subscription;
 
-  constructor(private _authService: AuthService) {
-    this.isAuthorized = this._authService.isAuthorized();
-  }
+  constructor(private _authService: AuthService,  private router: Router) {}
 
   ngOnInit() {
-    this._subscription = this._authService.subject.subscribe(res => this.isAuthorized = !!res);
+    // subscribe on userInfo
+    // this._subscription = this._authService.subject.subscribe(userInfo => this.userInfo = userInfo);
+
+    // check authorization
+    let isAuthorized = this._authService.isAuthorized()
+    if(isAuthorized) {
+      this._authService.readUserInfo();
+    } else {
+      this.router.navigate(['./login']);
+    }
   }
 
   ngOnDestroy() {
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { Subscription } from 'rxjs/Subscription';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../../shared/auth.service';
 import { User } from '../../shared/user';
@@ -14,15 +14,16 @@ export class HeaderComponent implements OnInit, OnDestroy {
     user: User;
     _subscription: Subscription;
 
-    constructor(private _authService: AuthService) {}
+    constructor(private _authService: AuthService,  private router: Router) {}
 
     ngOnInit() {
       this._subscription = this._authService.subject.subscribe(userInfo => this.user = userInfo);
-      this._authService.readUserInfo();
+      // this._authService.readUserInfo();
     }
 
     onLogoff() {
       this._authService.logout();
+      this.router.navigateByUrl('login');
     }
 
     ngOnDestroy() {
