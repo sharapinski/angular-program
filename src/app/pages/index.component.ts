da74948d3af19e59a17dc56e200b1981c1ee1d80
Raman <dzin13@tut.by>
2018-02-07 09:17:24 +0300

    fix pagination

--- src/app/pages/index.component.ts
+++ src/app/pages/index.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
+import { Component } from '@angular/core';
+// import { Subscription } from 'rxjs/Subscription';
 
 import { Course } from '../shared/course';
 import { CourseService } from '../shared/course.service';
@@ -11,69 +11,69 @@ import { SearchPipe } from '../shared/search.pipe';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent {
-  courses: Course[] = [];
-  searchValue: string = '';
-  showAddCourse: boolean = false;
-  _mainThread: Subscription;
-  page = {current: 0, total: 0};
-  editItem: Course
+  // courses: Course[] = [];
+  // searchValue: string = '';
+// page = {current: 0, total: 0};
+  // _mainThread: Subscription;
+  // page = {current: 0, total: 0};
+  // editItem: Course
 
-  constructor(private _service: CourseService) {}
+  constructor() {}
 
-  ngOnInit() {
-    this._mainThread = this._service.getList()
-                .map(res => {
-                  let page = res.facets && res.facets.pages ? res.facets.pages: null;
-                  let list = res.courses ? res.courses.filter(item => new Date(item.date).getTime() > new Date().getTime() - 14 * 24 * 60 * 60 * 1000): [];
-                  return {page, list};
-                })
-                .subscribe(obj => {
-                  if(obj.page) {
-                    this.page = obj.page;
-                  }
-                  this.courses = this.courses.concat(obj.list)
-                });
-  }
+  // ngOnInit() {
+  //   this._mainThread = this._service.getList()
+  //               .map(res => {
+  //                 let page = res.facets && res.facets.pages ? res.facets.pages: null;
+  //                 let list = res.courses ? res.courses.filter(item => new Date(item.date).getTime() > new Date().getTime() - 14 * 24 * 60 * 60 * 1000): [];
+  //                 return {page, list};
+  //               })
+  //               .subscribe(obj => {
+  //                 if(obj.page) {
+  //                   this.page = obj.page;
+  //                 }
+  //                 this.courses = this.courses.concat(obj.list)
+  //               });
+  // }
 
-  onSearch(str: string): void {
-    this.searchValue = str;
-    this.courses = [];
-    this.page.current = 0;
-    this.onShowMore();
-  }
-
-  onAddCourse() {
-    //this.editItem = new Course();
-    this.editItem = {
-      id: 0,
-      title: "",
-      description: "",
-      isTopRated: false,
-      date: new Date(),
-      // authors: [],
-      duration: undefined
-    };
-    this.showAddCourse = true;
-  }
-
-  onEdit(event) {
-    this.editItem = event;
-    this.showAddCourse = true;
-  }
-
-  onShowMore() {
-    this._service.getList(this.searchValue, this.page.current + 1);
-  }
-
-  onSave() {}
-
-  onCancel() {
-    this.showAddCourse = false;
-  }
-
-  ngOnDestroy() {
-    if(this._mainThread) {
-      this._mainThread.unsubscribe();
-    }
-  }
+  // onSearch(str: string): void {
+  //   this.searchValue = str;
+  //   this.courses = [];
+  //   this.page.current = 0;
+  //   this.onShowMore();
+  // }
+  //
+  // onAddCourse() {
+  //   //this.editItem = new Course();
+  //   this.editItem = {
+  //     id: 0,
+  //     title: "",
+  //     description: "",
+  //     isTopRated: false,
+  //     date: new Date(),
+  //     // authors: [],
+  //     duration: undefined
+  //   };
+  //   this.showAddCourse = true;
+  // }
+  //
+  // onEdit(event) {
+  //   this.editItem = event;
+  //   this.showAddCourse = true;
+  // }
+  //
+  // onShowMore() {
+  //   this._service.getList(this.searchValue, this.page.current + 1);
+  // }
+  //
+  // onSave() {}
+  //
+  // onCancel() {
+  //   this.showAddCourse = false;
+  // }
+  //
+  // ngOnDestroy() {
+  //   if(this._mainThread) {
+  //     this._mainThread.unsubscribe();
+  //   }
+  // }
 }
